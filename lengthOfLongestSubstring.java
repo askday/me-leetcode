522a154f35b827589cceea8c8fdf3439c99c49d0
天天 <xiangbo-99@163.com>
2018-12-25 10:54:32 +0800

    lengthOfLongestSubstring

--- lengthOfLongestSubstring.java
+++ lengthOfLongestSubstring.java
@@ -1,26 +1,32 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 class Solution {
   public static void main(String[] args) {
+    long start = System.currentTimeMillis();
     Solution solution = new Solution();
-    String testStr = "abcabcbb";
+    String testStr = " ";
     int lengthOfLongestSubstring = solution.lengthOfLongestSubstring(testStr);
     System.out.println(lengthOfLongestSubstring);
+    System.out.println(System.currentTimeMillis() - start);
   }
 
   public int lengthOfLongestSubstring(String str) {
     int result = -1;
-    Map<String, Integer> map = new HashMap<String, Integer>();
-    char[] chars = str.toCharArray();
-
-    for (int i = 0; i < chars.length; i++) {
-      char c = chars[i];
-      // map.put(c, 1);
-      while (true) {
-
+    Set<Character> set = new HashSet<Character>();
+    for (int i = 0; i < str.length(); i++) {
+      set.add(str.charAt(i));
+      for (int j = i + 1; j < str.length(); j++) {
+        if (set.contains(str.charAt(j))) {
+          result = Math.max(result, set.size());
+          set.clear();
+          break;
+        }
+        set.add(str.charAt(j));
       }
     }
-    return result;
+    return Math.max(result, set.size());
   }
 }
